f454bb94f6bd9a0a16baf562b1b0117ba9224bb8
Hyeezzzzzz <murilinho1002000@gmail.com>
2025-05-17 19:23:14 -0300

    Add .next/static/chunks/src_app_page_tsx_769d9e68._.js 1747520593724

---
+++ .next/static/chunks/src_app_page_tsx_769d9e68._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_0a4337e4._.js",
+  "static/chunks/src_91dbf8c7._.js"
+],
+    source: "dynamic"
+});
